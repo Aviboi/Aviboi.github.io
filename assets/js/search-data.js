f6312b855584890df9d087dfb80ca998d51a4c64
Aviboi <Aviboi@users.noreply.github.com>
2026-03-21 21:09:45 +0000

    Deploying to gh-pages from @ Aviboi/Aviboi.github.io@cdf82e029827aa8e9ed59969518ac5fe7f5e880f 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,57 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-projects",
+          title: "projects",
+          description: "A short collection of relevant projects, both personal and for my degree.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "projects-engrd-2020-nutcracker-design",
+          title: 'ENGRD 2020 Nutcracker Design',
+          description: "A short design project as part of my introductory statics course at Cornell.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/ENGRD_2020_Nutcracker_Design.html";
+            },},{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
